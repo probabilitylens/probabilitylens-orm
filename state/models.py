72bb636a87fcd9a482fa7a1687f65950a941f50c
probabilitylens <crazydealzs666@gmail.com>
2026-03-26 18:50:26 -0500

    Update models.py

--- state/models.py
+++ state/models.py
@@ -1,1 +1,9 @@
+class EvaluateRequest:
+    def __init__(self, params):
+        self.params = params
 
+
+class ComputedState:
+    def __init__(self, positions=None, equity=0):
+        self.positions = positions
+        self.equity = equity
