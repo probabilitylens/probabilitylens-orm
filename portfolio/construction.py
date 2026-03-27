9e85bc50edba99b4ff4d45d30413b675cc9eb5d0
probabilitylens <crazydealzs666@gmail.com>
2026-03-26 21:17:11 -0500

    Update construction.py

--- portfolio/construction.py
+++ portfolio/construction.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-def build_portfolio(signals, prices, capital, config):
+
+def construct_portfolio(signals, prices, capital, config):
     w = signals.clip(-config["max_weight"], config["max_weight"])
-    norm = w.abs().sum(axis=1).replace(0,1)
+    norm = w.abs().sum(axis=1).replace(0, 1)
     w = w.div(norm, axis=0)
 
     pos = (w * capital) / prices
-    pos = pos.replace([np.inf,-np.inf],0).fillna(0)
+    pos = pos.replace([np.inf, -np.inf], 0).fillna(0)
 
     return w, pos
